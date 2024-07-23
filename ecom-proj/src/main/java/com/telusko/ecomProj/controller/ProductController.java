7e2c0e7c0b50e12b6e9030dc91f2e7f678bbbe44
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-23 11:54:01 +0530

    Ecom Project Started with Intial Project Setup

---
+++ ecom-proj/src/main/java/com/telusko/ecomProj/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.telusko.ecomProj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+	
+	@RequestMapping("/")
+	public String greet()
+	{
+		return "Entered into Api successfully";
+	}
+
+}
---
+++ ecom-proj/src/main/java/com/telusko/ecomProj/model/Product.java
@@ -0,0 +1,30 @@
+package com.telusko.ecomProj.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+	
+	@Id
+	private int id;
+	private String name;
+	private String desc;
+	private String brand;
+	private BigDecimal price;
+	private String category;
+	private Date releasedDate;
+	private boolean available;
+	private int quantity;
+
+}
