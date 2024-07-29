54263f8a6066a87c48f93aee4ca9966ac9821359
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-07-29 20:31:52 +0530

    Changes

--- ecom-proj/src/main/java/com/telusko/ecomProj/controller/ProductController.java
+++ ecom-proj/src/main/java/com/telusko/ecomProj/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,7 +57,7 @@ public class ProductController {
 
 //---Add New Product -------------------------------------------------	
 	
-	@PutMapping("/product")
+	@PostMapping("/product")
 	public ResponseEntity<?> addProduct(@RequestPart Product product,
 										@RequestPart MultipartFile imageFile)
 	{
